7045905b478ec7283d323382ec4c5473d1d2ddbb
Will Drexler <wdrexler@mojolingo.com>
2013-07-05 17:13:57 -0400

    Fleshed out the CPS scenario

--- scenarios/cps.rb
+++ scenarios/cps.rb
@@ -1,7 +1,19 @@
 require 'sippy_cup'
 
-scenario = SippyCup::Scenario.new do |s|
+scenario = SippyCup::Scenario.new 'CPS', source: "127.0.0.1:8836", destination: "127.0.0.1:5060"  do |s|
+  s.invite
+  s.receive_trying
+  s.receive_ringing
+  s.receive_progress
 
+  s.receive_answer
+  s.ack_answer
+
+  #Sleep for the audio playback
+  s.sleep 2
+
+  s.receive_bye
+  s.ack_bye
 end
 
 scenario.compile!
