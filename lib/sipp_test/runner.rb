37403f7b197ae3483d146814786e873b596406fe
Will Drexler <wdrexler@mojolingo.com>
2013-07-05 15:35:43 -0400

    Added a basic outline and a #initialize method for the test runner

--- lib/sipp_test/runner.rb
+++ lib/sipp_test/runner.rb
@@ -1,6 +1,33 @@
 module SippTest
   class Runner
+    attr_reader :sipp_pid
+    attr_accessor :status
     def initialize(type)
+      @type = type
+      raise ArgumentError("Type #{@type} not supported!") unless @type == :concurrent || @type == :cps
+      @scenario_path = "#{Adhearsion.config[:platform].root}/#{Adhearsion.config[:sipp_test][@type].scenario_location}.xml"
+      @csv_path = "#{Adhearsion.config[:platform].root}/scenarios/#{Time.new.strftime("%Y%m%d%H%M%S")}-#{@type}.csv"
+      @extension = get_extension @type
+      self.status = :stopped
+    end
+
+    def run
+      case @type
+      when :concurrent
+        run_concurrent
+      when :cps
+        run_cps
+      end
+    end
+
+    def run_concurrent
+
+    end
+
+    def run_cps
+    end
+
+    def get_extension(type)
     end
   end
 end
