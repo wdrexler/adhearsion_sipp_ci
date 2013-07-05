7b7df0175d12cc4202419cfbd9fe0ff3f3e1a75b
Will Drexler <wdrexler@mojolingo.com>
2013-07-05 17:16:47 -0400

    Fleshed out the Concurrent scenario

--- scenarios/concurrent.rb
+++ scenarios/concurrent.rb
@@ -1,7 +1,19 @@
 require 'sippy_cup'
 
-scenario = SippyCup::Scenario.new do |s|
+scenario = SippyCup::Scenario.new 'Concurrent', source: "127.0.0.1:8836", destination: "127.0.0.1:5060" do |s|
+  s.invite
+  s.receive_trying
+  s.receive_ringing
+  s.receive_progress
 
+  s.receive_answer
+  s.ack_answer
+
+  #Sleep for the audio playback
+  s.sleep Adhearsion.config[:sipp_test].concurrent.call_length
+
+  s.send_bye
+  s.receive_200
 end
 
 scenario.compile!
