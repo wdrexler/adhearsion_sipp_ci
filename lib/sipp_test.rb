1ab61a7591099d909213d67193ea6cf1cd58eda6
Will Drexler <wdrexler@mojolingo.com>
2013-07-05 16:56:44 -0400

    Add new classes to `lib/sipp_test.rb`

--- lib/sipp_test.rb
+++ lib/sipp_test.rb
@@ -1,4 +1,6 @@
 SippTest = Module.new
 require "sipp_test/version"
 require "sipp_test/plugin"
-require "sipp_test/controller_methods"
+require "sipp_test/data_parser"
+require "sipp_test/watcher"
+require "sipp_test/runner"
