4b46500d7aac786c2bbbdf771f5208204b5fd517
Will Drexler <wdrexler@mojolingo.com>
2013-07-05 16:55:45 -0400

    Move DataParser into the SippTest module

--- lib/sipp_test/data_parser.rb
+++ lib/sipp_test/data_parser.rb
@@ -1,26 +1,27 @@
 require 'csv'
-
-class DataParser
-  class << self
-    def parse(path)
-      elapsed = []
-      total = []
-      success = []
-      failed = []
-      step = 0
-      if File.exists?(path)
-        CSV.foreach(path, :headers => true, :col_sep => ";") do |row|
-          total_step = row["TotalCallCreated"].to_i
-          success_step = row["SuccessfulCall(C)"].to_i
-          failed_step = row["FailedCall(C)"].to_i
-          elapsed << [step, row["ElapsedTime(C)"]]
-          total << [step, total_step]
-          success << [step, success_step]
-          failed << [step, failed_step]
-          step += 1
+module SippTest
+  class DataParser
+    class << self
+      def parse(path)
+        elapsed = []
+        total = []
+        success = []
+        failed = []
+        step = 0
+        if File.exists?(path)
+          CSV.foreach(path, :headers => true, :col_sep => ";") do |row|
+            total_step = row["TotalCallCreated"].to_i
+            success_step = row["SuccessfulCall(C)"].to_i
+            failed_step = row["FailedCall(C)"].to_i
+            elapsed << [step, row["ElapsedTime(C)"]]
+            total << [step, total_step]
+            success << [step, success_step]
+            failed << [step, failed_step]
+            step += 1
+          end
         end
+        {time: elapsed, series: [total, success, failed]}
       end
-      {time: elapsed, series: [total, success, failed]}
     end
   end
 end
